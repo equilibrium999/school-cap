a6dcf22adb3961d0942e19e140a6dda0b3ef0d2a
Balbino Soares Ferreira Filho <c0002867@cenibra.com.br>
2020-02-15 09:51:36 -0300

    adjust comments

--- srv/student-service.js
+++ srv/student-service.js
@@ -2,8 +2,6 @@
  * Implementation for StudentService defined in ./student-service.cds
  */
 
-
-
 module.exports = (srv) => {
 
   srv.before ('CREATE','Registrations', async (req) => {
@@ -11,22 +9,24 @@ module.exports = (srv) => {
     //Get transaction of the request
     const tx = cds.transaction(req);
 
-    //Search using Query converted to CQL
+    //Search using CQL, parsing to CQN
+    //MOre infos in https://cap.cloud.sap/docs/cds/cql
     let selectQuery = "SELECT from school.Registrations{count(student_ID) as count} where course_ID = " + req.data.course_ID;
     let query = cds.parse.cql(selectQuery);
     let studentCount = await tx.run(query);
     studentCount = studentCount[0];
     
-    //Using CQL to read Courses
+    //Using CQN to read Courses
+    //More infos in https://cap.cloud.sap/docs/cds/cqn
     let courses = await tx.read('school.Courses').where({ID:req.data.course_ID});
     let classRoomId = courses[0].classRoom_ID;
 
-    // Using CQL SELECT to read maxClassRoom
+    // Using CQN SELECT to read maxClassRoom
     let maxClassRoom = await tx.run(SELECT('maxStudents').from('school.Classrooms').where({ID:classRoomId}));
     maxClassRoom = maxClassRoom[0];
 
     //Check Max Number of Students in ClassRoom
-    if(studentCount.count > maxClassRoom.maxStudents)
+    if((studentCount.count + 1) > maxClassRoom.maxStudents)
       req.error(409,'Turma cheia');
   	
   })
